2b73ceffeea4b2ac857c4ca494fdc13784773dfe
d.mogylat <danylo.mogylat@delaware.com>
2018-11-26 12:18:16 +0200

    Configurations for SAP

--- src/main/java/com/mogydan/similarity/SimilarityApplication.java
+++ src/main/java/com/mogydan/similarity/SimilarityApplication.java
@@ -2,9 +2,16 @@ package com.mogydan.similarity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SimilarityApplication {
+public class SimilarityApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SimilarityApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SimilarityApplication.class, args);
--- src/main/java/com/mogydan/similarity/service/ProductServiceImpl.java
+++ src/main/java/com/mogydan/similarity/service/ProductServiceImpl.java
@@ -22,7 +22,7 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public void addProducts(List<Product> products) {
-        productRepository.saveAll(products);
+        productRepository.save(products);
     }
 
     @Override
