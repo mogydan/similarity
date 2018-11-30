086033181fc5a0c30cfc5684edb3a5fafd746b8b
d.mogylat <danylo.mogylat@delaware.com>
2018-11-30 17:17:54 +0200

    Modifying of DataPreparation controller

--- src/main/java/com/mogydan/similarity/controller/DataPreparationController.java
+++ src/main/java/com/mogydan/similarity/controller/DataPreparationController.java
@@ -4,17 +4,18 @@ import com.mogydan.similarity.model.Customer;
 import com.mogydan.similarity.model.OrderDetails;
 import com.mogydan.similarity.model.Product;
 import com.mogydan.similarity.model.PurchaseOrder;
-import com.mogydan.similarity.repository.CustomerRepository;
-import com.mogydan.similarity.repository.OrderDetailsRepository;
-import com.mogydan.similarity.repository.ProductRepository;
-import com.mogydan.similarity.repository.PurchaseOrderRepository;
-import io.swagger.annotations.Api;
+import com.mogydan.similarity.service.CustomerService;
+import com.mogydan.similarity.service.OrderDetailsService;
+import com.mogydan.similarity.service.ProductService;
+import com.mogydan.similarity.service.PurchaseOrderService;
+import io.swagger.annotations.*;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -28,49 +29,77 @@ import java.util.stream.IntStream;
 )
 public class DataPreparationController {
 
-    private final PurchaseOrderRepository purchaseOrderRepository;
-    private final CustomerRepository customerRepository;
-    private final ProductRepository productRepository;
-    private final OrderDetailsRepository orderDetailsRepository;
+    private final PurchaseOrderService purchaseOrderService;
+    private final CustomerService customerService;
+    private final ProductService productService;
+    private final OrderDetailsService orderDetailsService;
 
     @PostMapping("/data")
-    public void prepareData() {
-        List<Customer> customers = new ArrayList<>();
-        List<Product> products = new ArrayList<>();
-        for (int i = 0; i < 20; i++) {
-            customers.add(new Customer().setEmail("customer" + i + "@m.com").setName("customer" + i));
-            products.add(new Product().setColor("asd" + i).setName("product" + i).setPrice(123 + i));
-        }
-        customerRepository.save(customers);
-        productRepository.save(products);
-        List<PurchaseOrder> orders = IntStream.range(0, products.size())
-                .mapToObj(i -> new PurchaseOrder().setCustomer(customers.get(i)))
+    @ApiOperation(value = "Prepare new test Data", tags = {"Test Data Provider"})
+    @ApiResponses(value = @ApiResponse(code = 201, message = "OK"))
+    public void prepareData(
+            @ApiParam(value = "Product amount", required = true) @Valid @RequestParam("Products amount") int productAmount,
+            @ApiParam(value = "Customers amount", required = true) @Valid @RequestParam("Customers amount") int customersAmount,
+            @ApiParam(value = "Orders amount", required = true) @Valid @RequestParam("Orders amount") int orderAmount
+    ) {
+        List<Customer> customers = IntStream.range(0, customersAmount)
+                .mapToObj(i -> new Customer()
+                        .setEmail("customer" + i + "@m.com")
+                        .setName("customer" + i)
+                )
                 .collect(Collectors.toList());
 
-        purchaseOrderRepository.save(orders);
 
-        List<OrderDetails> orderDetails = IntStream.range(0, customers.size())
-                .mapToObj(i -> {
-                    Set<OrderDetails> items = new HashSet<>();
-                    for (int j = 0; j < new Random().nextInt(products.size() - 1); j++) {
-                        OrderDetails item = new OrderDetails()
-                                .setProduct(products.get(new Random().nextInt(products.size() - 1)))
-                                .setPurchaseOrder(orders.get(new Random().nextInt(orders.size() - 1)))
-                                .setAmount((long) j);
-                        items.add(item);
-                    }
-                    return items;
-                })
+        List<Product> products = IntStream.range(0, productAmount)
+                .mapToObj(i -> new Product()
+                        .setColor("color" + i)
+                        .setName("product" + i)
+                        .setPrice(new Random().nextInt(100) + i)
+                )
+                .collect(Collectors.toList());
+
+        customerService.addCustomers(customers);
+        productService.addProducts(products);
+
+
+        List<PurchaseOrder> orders = IntStream.range(0, orderAmount)
+                .mapToObj(i -> new PurchaseOrder()
+                        .setCustomer(customers.get(
+                                new Random()
+                                        .nextInt(customers.size() - 1)
+                                )
+                        )
+                )
+                .collect(Collectors.toList());
+
+        purchaseOrderService.addOrders(orders);
+
+
+        List<OrderDetails> orderDetails = orders.stream()
+                .map(order -> {
+                            Set<OrderDetails> items = new HashSet<>();
+                            for (int j = 0; j < new Random().nextInt(products.size() - 1); j++) {
+                                OrderDetails item = new OrderDetails()
+                                        .setProduct(products.get(j))
+                                        .setPurchaseOrder(order)
+                                        .setAmount(new Random().nextInt(20) + 1);
+                                items.add(item);
+                            }
+                            return items;
+                        }
+                )
                 .flatMap(Collection::stream)
                 .collect(Collectors.toList());
 
-        orderDetailsRepository.save(orderDetails);
+        orderDetailsService.addListOfOrderDetails(orderDetails);
 
     }
 
-    @GetMapping("/orders/all")
-    public @ResponseBody
-    List<PurchaseOrder> getAllOrders() {
-        return purchaseOrderRepository.findAll();
+    @DeleteMapping("/data/clear")
+    public void clear() {
+        orderDetailsService.clear();
+        purchaseOrderService.clear();
+        productService.clear();
+        customerService.clear();
     }
 }
